16efabc85e56691a8252278a251291636a7f765a
dang <110548556+hongdangn@users.noreply.github.com>
2023-06-04 01:06:27 +0700

    Create all5digitsnum.cpp

---
+++ all5digitsnum.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using    namespace   std;
+int d[100000];
+void   printdigits(int num, int d[], int cnt)
+{
+    if(cnt == 5)
+    {
+        cout << num << " ";
+        return;
+    }
+    d[num] = 1;
+    for(int i = 0; i <= 9; i++)
+    {
+        if(d[i] == 0)
+        {
+            d[i] = 1;
+            printdigits(num*10 + i, d, cnt + 1);
+            d[i] = 0;
+        }
+    }
+    d[num] = 0;
+}
+int main()
+{
+    for(int i = 1; i <= 9; i++)
+    {
+        printdigits(i, d, 1);
+    }
+}
